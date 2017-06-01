d17e2729f41970fd261270957aa34e3c4a4ba17e
Khor Shao Liang <khorshaoliang@Khors-Air.home>
2017-06-01 23:19:22 +0800

    EventForm Progress_v1. Simple data storing. User file upload, googlemap unresolved.

--- client/javascript/eventform.js
+++ client/javascript/eventform.js
@@ -1,6 +1,26 @@
 UserEvents = new Mongo.Collection('userEvents');
 
+Images = new FS.Collection('Images', {
+	stores: [new FS.Store.GridFS('Images')],
+		filter: {
+			allow: {
+				contentTypes: ['image/*']
+			},
+			onInvalid: function(message) {
+				FlashMessage.sendError(message);
+			}
+		}
+});
+
+Images.allow({
+	insert: function(){ return true;},
+	update: function(){ return true;},
+	download: function(){ return true;},
+})
+
 if(Meteor.isClient) {
+	Meteor.subscribe("userEvents");
+
 	Template.eventform.helpers({
 		userEvents: function() {
 			if (Session.get('done')) {
@@ -8,28 +28,69 @@ if(Meteor.isClient) {
 			} else {
 				return UserEvents.find();
 			}
-		}
+		},
+
+		images: function() {
+			return Images.find();
+		},
+
+		exampleMapOptions: function() {
+	    // Make sure the maps API has loaded
+	    if (GoogleMaps.loaded()) {
+	      // Map initialization options
+	      return {
+	        center: new google.maps.LatLng(1.3521, 103.8198),
+	        zoom: 10
+	      };
+	    }
+	  }
+	});
+
+	Template.eventform.onCreated(function() {
+	  // We can use the `ready` callback to interact with the map API once the map is ready.
+	  GoogleMaps.ready('exampleMap', function(map) {
+	    // Add a marker to the map once it's ready
+	    var marker = new google.maps.Marker({
+	      position: map.options.center,
+	      map: map.instance
+	    });
+	  });
+	});
+
+	Template.eventform.onRendered(function() {
+	  GoogleMaps.load();
 	});
 
 	Template.eventform.events({
 		'submit .new-event': function(event) {
 			var title = event.target.title.value;
 			var description = event.target.description.value;
+			var location = event.target.location.value;
+			var dateTime = event.target.dateTime.value;
+			var type = event.target.type.value;
+			var privacy = event.target.privacy.value;
 
-			UserEvents.insert({
-				title: title,
-				description: description,
-				createdAt: new Date()
-			});
-
-			event.target.title.value = "";
-			event.target.description.value = "";
+			Meteor.call("addEvent", title, description, location, dateTime, type, privacy);
 
 			return false;
 		},
 
 		'change .hide-finished': function(event) {
 			Session.set('done', event.target.checked);
+		},
+
+		'change .img-input': function(event) {
+			console.log(event);
+			FS.Utility.eachFile(event, function(file){
+				var newFile = new FS.File(file);
+				Images.insert(newFile, function(error, result){
+					if(error) {
+						console.log('There is an issue with the upload');
+					} else {
+						console.log('Image Uploaded');
+					}
+				});
+			});
 		}
 	});
 
@@ -42,7 +103,7 @@ if(Meteor.isClient) {
 		},
 
 		'click .delete': function() {
-			UserEvents.remove(this._id);
+			Meteor.call("removeEvent", this._id);
 		}
 	});
 }
--- server/main.js
+++ server/main.js
@@ -2,6 +2,42 @@ import { Meteor } from 'meteor/meteor';
 
 UserEvents = new Mongo.Collection('userEvents');
 
+Images = new FS.Collection('Images', {
+	stores: [new FS.Store.GridFS('Images')],
+		filter: {
+			allow: {
+				contentTypes: ['image/*']
+			},
+			onInvalid: function(message) {
+				console.log(message);
+			}
+		}
+});
+
 Meteor.startup(() => {
   // code to run on server at startup
+});
+
+if(Meteor.isServer) {
+	Meteor.publish("userEvents", function() {
+		return UserEvents.find();
+	});
+}
+
+Meteor.methods({
+	addEvent: function(title, description, location, dateTime, type, privacy){
+		UserEvents.insert({
+			// lacking img, owner
+			title: title,
+			description: description,
+			location: location,
+			dateTime: dateTime,
+			type: type,
+			privacy: privacy,
+			createdAt: new Date()
+		});
+	},
+	removeEvent: function(id){
+		UserEvents.remove(id);
+	}
 });
